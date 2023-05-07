1eeee86cd8d5eb470aa5be653d532eb57809eab7
Klinton-VM <100642525+Klintonjmd@users.noreply.github.com>
2023-05-07 16:40:25 -0400

    Complete 2, 3, and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (input) {
+  let res = "";
+  for (let i = input.length; i > -1; i--) {
+    res += input.charAt(i);
+  }
+  return res;
 };
 
 // Do not edit below this line
